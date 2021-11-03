58f8f531dec5d1834092fb83f9fff3d71b30c4b6
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-03 13:16:01 +0800

    Test Passed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "xxx";
+
+function upperCaseCustomerName() {
+	customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+	window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+	bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+	leastFavoriteCustomer = "yyy";
+}
